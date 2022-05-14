ecb293df780cbefd44ad9a6251e7d99966fef3f0
perArdua <70432496+perArdua@users.noreply.github.com>
2022-05-14 15:30:38 +0900

    0512_숙제

--- 김태호/05_12/BOJ_15663.py
+++ 김태호/05_12/BOJ_15663.py
@@ -28,28 +28,3 @@ def recursion(lst):
 
 
 recursion(list())
-# print(total)
-
-# import sys
-# from collections import Counter
-
-# sys.stdin = open('input.txt', 'r')
-# n,m = map(int,input().split())
-# temp = list(map(int,input().split()))
-# temp = list(Counter(temp).items())
-# temp.sort()
-# n = len(temp)
-# num,cnt = map(list,zip(*temp))
-
-# a = [0]*m
-# def go(index, n, m):
-#     if index == m:
-#         sys.stdout.write(' '.join(map(str,a))+'\n')
-#         return
-#     for i in range(n):
-#         if cnt[i] > 0:
-#             cnt[i] -= 1
-#             a[index] = num[i]
-#             go(index+1, n, m)
-#             cnt[i] += 1
-# go(0,n,m)
