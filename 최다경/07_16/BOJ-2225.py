a4cfec4c6e2c830a0674543abd330137fda337f5
Dagyeong Choi <99407953+ekrud99@users.noreply.github.com>
2022-07-16 10:58:02 +0900

    07_16_숙제

--- 최다경/07_16/BOJ-2225.py
+++ 최다경/07_16/BOJ-2225.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 MOD = int(1e9)
 dp = [1 for _ in range(n + 1)]
 
-#dp[i] :=  / dp는 이전단계의 현재 index에 해당하는 수 + 현재단계의 이전 index에 해당하는 수
+#dp[i] := 0 ~ m개의 숫자로 n을 만들 수 있는 경우의 수의 배열 / dp는 이전단계의 현재 index에 해당하는 수 + 현재단계의 이전 index에 해당하는 수
 dp[0] = 1
 for i in range(m - 1):
     for j in range(1, n + 1):
