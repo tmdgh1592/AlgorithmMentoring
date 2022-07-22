bda3e04cb5f24c1e7248ba4e012147d0edea2c1d
Jung So Young <soyoung5611@gmail.com>
2022-07-22 23:32:33 +0900

    0712_숙제

--- 정소영/07_12/BOJ_15990.py
+++ 정소영/07_12/BOJ_15990.py
@@ -1,5 +1,4 @@
 #-*- coding:utf-8 -*-
-from lzma import MODE_FAST
 import sys
 
 sys.stdin = open('input.txt', 'r')
@@ -10,7 +9,7 @@ n = int(input())
 
 MOD = int(1e9) + 9
 
-dp = [[0] * 4 for _ in range(100001)]
+dp = [[0] * 4 for _ in range(int(1e5) + 1)]
 
 #dp[i][j] : i = 정수 n을 구하는 경우의 수, j = 1,2,3 중 마지막으로 사용한 수
 #dp[i][1] = dp[i - 1][2] + dp[i - 1][3] : 마지막으로 사용한 숫자는 1이므로 2, 3을 이용 할 수 있다 
