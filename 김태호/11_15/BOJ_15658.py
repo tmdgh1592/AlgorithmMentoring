1003ec6fdc7c6999c9dc00a2d2e7ff70a0eb1b89
perArdua <70432496+perArdua@users.noreply.github.com>
2022-11-15 13:09:27 +0900

    1115_숙제

--- 김태호/11_15/BOJ_15658.py
+++ 김태호/11_15/BOJ_15658.py
@@ -1,7 +1,31 @@
 #-*- coding:utf-8 -*-
 import sys
+from operator import add, mul, sub, truediv
 
-sys.stdin = open('input.txt', 'r')
+# sys.stdin = open('input.txt', 'r')
 input = sys.stdin.readline
 MIS = lambda: map(int, input().rstrip().split())
 
+div = lambda x, y: int(truediv(x, y))
+operator = {1 : sub, 2 : mul, 3 : div, 0 : add}
+
+def f(idx, res):
+    if idx == n:
+        return res, res
+        
+    max_val = -float('inf')
+    min_val = float('inf')
+    for i in range(4):
+        if oper[i]:
+            oper[i] -= 1
+            ret = f(idx + 1, operator[i](res, num[idx]))
+            max_val = max(ret[0], max_val)
+            min_val = min(ret[1], min_val)
+            oper[i] += 1
+    return max_val, min_val
+
+n = int(input())
+num = list(MIS())
+oper = list(MIS())
+
+print(*f(1, num[0]), sep='\n')
