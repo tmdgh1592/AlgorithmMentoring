370a43f201e200f52a5327bc3ed67f53ccc08142
tmdgh1592 <tmdgh1592@naver.com>
2023-01-23 21:09:19 +0900

    백준 1339번 문제풀이

--- 최승호/23_01_19/BOJ_11659.cpp
+++ 최승호/23_01_19/BOJ_11659.cpp
@@ -11,14 +11,33 @@
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
+typedef long long int lli;
+
+int n, m;
+int vec[100000];
+int sums[100000];
+
+int interval_sum(int left, int right) {
+    return sums[right] - sums[left - 1];
+}
 
 int main(){
     FAST;
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
 #endif
+    cin >> n >> m;
 
+    rep(i, 0, n) {
+        cin >> vec[i];
+        sums[i + 1] = sums[i] + vec[i];
+    }
 
+    rep(i, 0, m) {
+        int left, right;
+        cin >> left >> right;
+        cout << interval_sum(left, right) << endl;
+    }
 
     return 0;
 }
