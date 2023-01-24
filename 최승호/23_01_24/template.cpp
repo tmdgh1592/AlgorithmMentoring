6994ef86dc5375499256d3d2fadc8d018d4a1001
tmdgh1592 <tmdgh1592@naver.com>
2023-01-24 15:33:51 +0900

    23_01_19 숙제

--- 최승호/23_01_24/template.cpp
+++ 최승호/23_01_24/template.cpp
@@ -12,6 +12,23 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
+const int MAX = 1'000'000;
+ll n;
+ll tree[MAX + 1];
+
+ll sum(int pos) {
+    ll ret = 0;
+    while(pos > 0) {
+        ret += tree[pos];
+        pos -= pos & -pos;
+    }
+    return ret;
+}
+
+ll update(int pos, ll val) {
+    
+}
+
 int main(){
     FAST;
 #ifndef ONLINE_JUDGE
