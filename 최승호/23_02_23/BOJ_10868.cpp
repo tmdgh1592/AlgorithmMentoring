a3d657d7d1628be5243269e617d8377e6eadde32
tmdgh1592 <tmdgh1592@naver.com>
2023-02-23 23:08:36 +0900

    백준 10868번 문제풀이

--- 최승호/23_02_23/BOJ_10868.cpp
+++ 최승호/23_02_23/BOJ_10868.cpp
@@ -1,19 +1,16 @@
 #include <bits/stdc++.h>
 #define endl "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define rep(i, a, b) for(auto i = a; i < b; ++i)
 #define REP(i, a, b) for(auto i = a; i <= b; ++i)
 #define pii pair<int, int>
 #define all(v) (v).begin(), (v).end()
 #define pb push_back
 #define INF numeric_limits<int>::max()
+#define PIV 1 << 20
 
 using namespace std;
 
-#ifdef ONLINE_JUDGE
-constexpr bool ndebug = true;
-#else
-constexpr bool ndebug = false;
-#endif
 #define FAST_IO \
     if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
 #define debug(x) \
@@ -26,12 +23,53 @@ constexpr bool ndebug = false;
 typedef long long ll;
 typedef unsigned long long ull;
 
+int n, k;
+ll tree[PIV << 1];
+
+void update(int pos, ll val) {
+    pos += PIV;
+    tree[pos] = val;
+    while(pos >>= 1) {
+        tree[pos] = min(tree[pos << 1], tree[(pos << 1) + 1]);
+    }
+}
+
+ll query(int l, int r) {
+    l += PIV, r += PIV;
+    ll ret = 1000000001;
+
+    while(l <= r) {
+        if (l & 1) {
+            ret = min(ret, tree[l++]);
+        }
+        if (~r & 1) {
+            ret = min(ret, tree[r--]);
+        }
+        l >>= 1, r >>= 1;
+    }
+    return ret;
+}
+
 int main(){
-    FAST_IO;
+    FAST;
 #ifndef ONLINE_JUDGE
     clock_t start = clock();
     freopen("input.txt", "r", stdin);
 #endif
+    cin >> n >> k;
+    ll t;
+
+    REP(i, 1, n) {
+        cin >> t;
+        update(i, t);
+    }
+
+    int l, r;
+    while(k--) {
+        cin >> l >> r;
+        if (l > r) swap(l, r);
+        cout << query(l, r) << endl;
+    }
 
 #ifndef ONLINE_JUDGE
     cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
