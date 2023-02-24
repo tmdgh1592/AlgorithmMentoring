553a59308fb246c1061a92d2398df4a357969811
tmdgh1592 <tmdgh1592@naver.com>
2023-02-24 10:27:28 +0900

    update template.cpp

--- 최승호/etc/template.cpp
+++ 최승호/etc/template.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 #define endl "\n"
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
 #define rep(i, a, b) for(auto i = a; i < b; ++i)
 #define REP(i, a, b) for(auto i = a; i <= b; ++i)
 #define pii pair<int, int>
@@ -9,11 +10,6 @@
 
 using namespace std;
 
-#ifdef ONLINE_JUDGE
-constexpr bool ndebug = true;
-#else
-constexpr bool ndebug = false;
-#endif
 #define FAST_IO \
     if constexpr (ndebug) { cin.tie(nullptr); ios::sync_with_stdio(false); }
 #define debug(x) \
@@ -27,11 +23,13 @@ typedef long long ll;
 typedef unsigned long long ull;
 
 int main(){
-    FAST_IO;
+    FAST;
 #ifndef ONLINE_JUDGE
     clock_t start = clock();
     freopen("input.txt", "r", stdin);
 #endif
+
+
 
 #ifndef ONLINE_JUDGE
     cout << endl << "elapsed time: " << static_cast<double>(clock() - start) / CLOCKS_PER_SEC << "ms" << endl;
